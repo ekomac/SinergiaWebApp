d99005daf0ff381b5c51906f507add7fbe181d84
Juan Cruz Maciel Henning <jcmacielhenning@gmail.com>
2021-09-29 18:31:35 -0300

    created api folders for places

---
+++ src/places/api/serializers.py
@@ -0,0 +1,8 @@
+from rest_framework import serializers
+from places.models import Town
+
+
+class TownSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Town
+        fields = ['name', 'partido', 'delivery_code', 'flex_code',]
