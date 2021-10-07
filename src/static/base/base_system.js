c05df02b3476377c29af96765045603a6820b9a4
Juan Cruz Maciel Henning <jcmacielhenning@gmail.com>
2021-10-07 19:57:55 -0300

    updated scroll to top button

--- src/static/base/base_system.js
+++ src/static/base/base_system.js
@@ -1,26 +1,42 @@
 $(document).ready(function() {
 
     //Get the button:
-    mybutton = document.getElementById("backToTopBtn");
+    backToTopBtn = document.getElementById("backToTopBtn");
 
     // When the user scrolls down 20px from the top of the document, show the button
     window.onscroll = function() {
         scrollFunction()
     };
 
-    function scrollFunction() {
-        if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
-            mybutton.style.display = "block";
+    /*$("main").scroll(function() { //.box is the class of the div
+        if (document.body.scrollTop > 10 || document.documentElement.scrollTop > 10) {
+            backToTopBtn.style.display = "block";
         } else {
-            mybutton.style.display = "none";
+            backToTopBtn.style.display = "none";
+        }
+    });*/
+
+    $("#not-header").scroll(function() {
+        if ($(this).scrollTop() >= 20) {
+            backToTopBtn.style.display = "block";
+        } else {
+            backToTopBtn.style.display = "none";
+        }
+    });
+
+    function scrollFunction() {
+        if (document.body.scrollTop > 10 || document.documentElement.scrollTop > 10) {
+            backToTopBtn.style.display = "block";
+        } else {
+            backToTopBtn.style.display = "none";
         }
     }
-
-
 });
 
 // When the user clicks on the button, scroll to the top of the document
 function topFunction() {
+    // $("#not-header").scrollTop(0);
+    document.getElementById("not-header").scrollTo({ top: 0, behavior: 'smooth' });
     document.body.scrollTop = 0; // For Safari
     document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
 }
