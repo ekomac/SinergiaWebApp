083b35af060f1f2b7c7f577d2d67a2fc467cfe0d
Juan Cruz Maciel Henning <jcmacielhenning@gmail.com>
2022-01-29 01:40:12 -0300

    backup

--- src/account/api/urls.py
+++ src/account/api/urls.py
@@ -1,6 +1,7 @@
 
 from django.urls import path
 from account.api.views import (
+    ObtainAuthTokenView,
     ApiCarrierListView,
     api_detail_carrier_view,
     ApiEmployeesWithEnviosListView,
@@ -10,7 +11,7 @@ from account.api.views import (
 app_name = 'account-api'
 
 urlpatterns = [
-
+    path('login', ObtainAuthTokenView.as_view(), name="login"),
     path('employees-with-envios/', ApiEmployeesWithEnviosListView.as_view(),
          name="employees-with-envios"),
     path('carriers/', ApiCarrierListView.as_view(),
--- src/account/api/views.py
+++ src/account/api/views.py
@@ -1,19 +1,53 @@
 # DJANGO
+from django.contrib.auth import authenticate
 from django.db.models import Count
 
 # REST FRAMEWORK
 from rest_framework import status
-from rest_framework.response import Response
-from rest_framework.decorators import api_view, permission_classes
-from rest_framework.permissions import IsAuthenticated
+
 from rest_framework.authentication import TokenAuthentication
-from rest_framework.pagination import PageNumberPagination
+from rest_framework.authtoken.models import Token
+from rest_framework.decorators import api_view, permission_classes
 from rest_framework.generics import ListAPIView
+from rest_framework.pagination import PageNumberPagination
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+from rest_framework.views import APIView
 
 
 # PROJECT
 from account.api.serializers import EmployeeSerializer
 from account.models import Account
+# LOGIN
+# Response: https://gist.github.com/mitchtabian/8e1bde81b3be342853ddfcc45ec0df8a
+# URL: http://127.0.0.1:8000/api/account/login
+
+
+class ObtainAuthTokenView(APIView):
+
+    authentication_classes = []
+    permission_classes = []
+
+    def post(self, request):
+        context = {}
+
+        email = request.POST.get('username')
+        password = request.POST.get('password')
+        account = authenticate(email=email, password=password)
+        if account:
+            try:
+                token = Token.objects.get(user=account)
+            except Token.DoesNotExist:
+                token = Token.objects.create(user=account)
+            context['response'] = 'Successfully authenticated.'
+            context['pk'] = account.pk
+            context['email'] = email.lower()
+            context['token'] = token.key
+        else:
+            context['response'] = 'Error'
+            context['error_message'] = 'Credenciales inválidas'
+
+        return Response(context)
 
 
 @api_view(['GET', ])
