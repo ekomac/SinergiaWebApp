3d4dfc98a4b54d95852b17abcd4b638804815477
Juan Cruz Maciel Henning <jcmacielhenning@gmail.com>
2022-04-26 19:24:00 -0300

    added movements section (visible to admins)

--- src/tracking/views.py
+++ src/tracking/views.py
@@ -1,4 +1,4 @@
-from datetime import datetime, timedelta
+from datetime import datetime
 from typing import Any, Dict
 from django.contrib.auth.mixins import LoginRequiredMixin
 from account.decorators import allowed_users_in_class_view
@@ -9,8 +9,10 @@ from utils.views import CompleteListView, sanitize_date
 
 
 class TrackingMovementsListView(CompleteListView, LoginRequiredMixin):
+
     template_name = 'tracking/list.html'
     model = TrackingMovement
+    include_add_button = False
     decoders = (
         {
             'key': 'min_date',
@@ -21,34 +23,41 @@ class TrackingMovementsListView(CompleteListView, LoginRequiredMixin):
         {
             'key': 'max_date',
             'filter': 'date_created__lte',
-            'function': lambda x: sanitize_date(x, True) + timedelta(days=1),
+            'function': lambda x: sanitize_date(x, True),
             'context': lambda x: x,
         },
         {
-            'key': 'from_deposit_id',
+            'key': 'from_deposit',
             'filter': lambda x: 'from_deposit__isnull' if (
                 x in [-1, '-1']) else 'from_deposit__id',
             'function': lambda x: True if x in [-1, '-1'] else int(x),
             'context': str,
         },
         {
-            'key': 'to_deposit_id',
+            'key': 'to_deposit',
             'filter': lambda x: 'to_deposit__isnull' if (
                 x in [-1, '-1']) else 'to_deposit__id',
             'function': lambda x: True if x in [-1, '-1'] else int(x),
             'context': str,
         },
         {
-            'key': 'from_carrier_id',
+            'key': 'from_carrier',
             'filter': lambda x: 'from_carrier__isnull' if (
                 x in [-1, '-1']) else 'from_carrier__id',
             'function': lambda x: True if x in [-1, '-1'] else int(x),
             'context': str,
         },
         {
-            'key': 'to_carrier_id',
+            'key': 'to_carrier',
             'filter': lambda x: 'to_carrier__isnull' if (
                 x in [-1, '-1']) else 'to_carrier__id',
+            'function': lambda x: True if x in [-1, '-1'] else int(x),
+            'context': str,
+        },
+        {
+            'key': 'author',
+            'filter': lambda x: 'created_by__isnull' if (
+                x in [-1, '-1']) else 'created_by__id',
             'function': lambda x: True if x in [-1, '-1'] else int(x),
             'context': str,
         },
@@ -60,16 +69,21 @@ class TrackingMovementsListView(CompleteListView, LoginRequiredMixin):
         },
     )
     query_keywords = (
+        'created_by__first_name__icontains',
+        'created_by__last_name__icontains',
+        'created_by__username__icontains',
+        'created_by__email__icontains',
+        'created_by__dni__icontains',
         'from_carrier__first_name__icontains',
-        'from_carrier_id__last_name__icontains',
-        'from_carrier_id__username__icontains',
-        'from_carrier_id__email__icontains',
-        'from_carrier_id__dni__icontains',
+        'from_carrier__last_name__icontains',
+        'from_carrier__username__icontains',
+        'from_carrier__email__icontains',
+        'from_carrier__dni__icontains',
         'to_carrier__first_name__icontains',
-        'to_carrier_id__last_name__icontains',
-        'to_carrier_id__username__icontains',
-        'to_carrier_id__email__icontains',
-        'to_carrier_id__dni__icontains',
+        'to_carrier__last_name__icontains',
+        'to_carrier__username__icontains',
+        'to_carrier__email__icontains',
+        'to_carrier__dni__icontains',
         'from_deposit__name__icontains',
         'from_deposit__client__name__icontains',
         'to_deposit__name__icontains',
@@ -82,6 +96,8 @@ class TrackingMovementsListView(CompleteListView, LoginRequiredMixin):
 
     def get_context_data(self) -> Dict[str, Any]:
         context = super().get_context_data()
+        context['results'] = TrackingMovement.RESULTS
+        context['users'] = Account.objects.all()
         context['deposits'] = Deposit.objects.all()
         context['carriers'] = Account.objects.filter(
             groups__name__in=['Admins', "Level 1", "Level 2"])
