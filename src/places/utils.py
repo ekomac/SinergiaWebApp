32c68db699232a2fc2af1672e8e8804c789c008d
Juan Cruz Maciel Henning <jcmacielhenning@gmail.com>
2022-05-31 14:59:12 -0300

    implemented feature where zones displays it's related partido's or town's names

--- src/places/utils.py
+++ src/places/utils.py
@@ -1,5 +1,7 @@
 import json
-from places.models import Town
+from typing import Dict, List
+from places.models import Partido, Town
+from prices.models import DeliveryCode
 
 
 def get_localidades_as_JSON():
@@ -14,3 +16,40 @@ def map_town_to_dict(town):
         'name': town.name.title(),
         'partido_id': town.partido.id,
     }
+
+
+def get_places_data_for_dcode(dcode: DeliveryCode) -> Dict[str, List[str]]:
+    result = {
+        'id': dcode.id,
+        'code': dcode.code,
+        'max_5k_price': dcode.max_5k_price,
+        'bulto_max_10k_price': dcode.bulto_max_10k_price,
+        'bulto_max_20k_price': dcode.bulto_max_20k_price,
+        'miniflete_price': dcode.miniflete_price,
+        'tramite_price': dcode.tramite_price,
+        "partidos": [],
+        "towns": []
+    }
+    for partido in Partido.objects.filter(
+            town__delivery_code__in=[dcode]).distinct():
+        print("Partido", partido.name)
+        towns = Town.objects.filter(partido=partido)
+        total_towns = len(towns)
+        print("total towns:", total_towns)
+        try:
+            if (total_towns > 0):
+                first_code = towns[0].delivery_code.code
+                with_same_code = list(filter(
+                    lambda x: x.delivery_code.code == first_code, towns))
+                print("with same code:", len(with_same_code))
+                if total_towns == len(with_same_code):
+                    result["partidos"].append(partido.name)
+                else:
+                    raise AttributeError("")
+        except AttributeError:
+            for town in towns:
+                result["towns"].append(town.name)
+    print()
+    print()
+    print()
+    return result
