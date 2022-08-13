203f694fcc5fd0f5b2375100acadf227c63c6823
Juan Cruz Maciel Henning <jcmacielhenning@gmail.com>
2022-08-13 00:10:27 -0300

    create urls for de/activate view functions for deposits

--- src/deposit/urls.py
+++ src/deposit/urls.py
@@ -5,7 +5,9 @@ from .views import (
     deposit_create_view,
     deposit_edit_view,
     deposit_detail_view,
-    deposit_delete_view
+    deposit_delete_view,
+    activate_deposit_view,
+    deactivate_deposit_view,
 )
 
 app_name = 'deposits'
@@ -14,5 +16,7 @@ urlpatterns = [
     path('add/', deposit_create_view, name="add"),
     path('<int:pk>/', deposit_detail_view, name="detail"),
     path('<int:pk>/edit/', deposit_edit_view, name="edit"),
+    path('<int:pk>/activate/', activate_deposit_view, name="activate"),
+    path('<int:pk>/deactivate/', deactivate_deposit_view, name="deactivate"),
     path('<int:pk>/delete/', deposit_delete_view, name="delete"),
 ]
