fa6d60ca0312b00fb3b6dbd93bc4c3b4dfe2b873
ekomac <jcmacielhenning@gmail.com>
2024-04-07 19:02:54 -0300

    Commented out delete buttons in client and deposit detail templates

--- src/deposit/urls.py
+++ src/deposit/urls.py
@@ -5,7 +5,7 @@ from .views import (
     deposit_create_view,
     deposit_edit_view,
     deposit_detail_view,
-    deposit_delete_view,
+    # deposit_delete_view,
     activate_deposit_view,
     deactivate_deposit_view,
 )
@@ -18,5 +18,5 @@ urlpatterns = [
     path('<int:pk>/edit/', deposit_edit_view, name="edit"),
     path('<int:pk>/activate/', activate_deposit_view, name="activate"),
     path('<int:pk>/deactivate/', deactivate_deposit_view, name="deactivate"),
-    path('<int:pk>/delete/', deposit_delete_view, name="delete"),
+    # path('<int:pk>/delete/', deposit_delete_view, name="delete"),
 ]
